bb08227464b222ae7bc481a88c974dcb929a736c
EuheniiaHavra <eugeniavad@gmail.com>
2019-01-09 11:27:10 +0100

    script added

---
+++ script/scrips.js
@@ -0,0 +1,6 @@
+
+$(function () {
+  $('#waterfall').NewWaterfall()
+})
+
+
---
+++ script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+	var btn = $('#button');
+
+	btn.on('click', function (e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, '300');
+	});
+})
+
+
+
+
